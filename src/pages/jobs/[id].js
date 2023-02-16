d42a879fbd459127e328e13b74ba03de94519b38
Mauro.V <remanomauro@gmail.com>
2023-02-16 15:51:21 +0100

    getServerSideProps

--- src/pages/jobs/[id].js
+++ src/pages/jobs/[id].js
@@ -1,8 +1,27 @@
-const JobListingPage = () => {
+import { useRouter } from "next/router"
+
+
+const JobListingPage = (props) => {
+  console.log(props)
+  const router = useRouter
   
+
   return(
     <div>JobListingPage</div>
   )
+}
+
+export async function getServerSideProps(context) {
+  console.log(context.params.id)
+  console.log(context)
+  return {
+    props: {
+      job: {
+        id: '123',
+        title: 'Amazing galaxies',
+      },
+    },
+  };
 }
 
 export default JobListingPage
